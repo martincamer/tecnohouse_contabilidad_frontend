2874756221f3c39ca8b432710cf7acfd092a7bf1
Martin <martincamer011@gmail.com>
2024-03-15 15:07:45 -0300

    Update GenerarRecibosEstadistica.jsx

--- src/routes/pages/protected/GenerarRecibosEstadistica.jsx
+++ src/routes/pages/protected/GenerarRecibosEstadistica.jsx
@@ -441,7 +441,7 @@ export const GenerarRecibosEstadistica = () => {
               </tbody>
             </table>
           </div>
-        )}
+        ))}
       </div>
 
       <ToastContainer />
