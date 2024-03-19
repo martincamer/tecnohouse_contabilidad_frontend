c88a36b3ef5c5c87feee8a462491ba9a30104e12
Martin <martincamer011@gmail.com>
2024-03-19 12:55:09 -0300

    Update ImprirmirEstadisticaPdf.jsx

--- src/components/pdf/ImprirmirEstadisticaPdf.jsx
+++ src/components/pdf/ImprirmirEstadisticaPdf.jsx
@@ -278,113 +278,113 @@ export const ImprimirEstadisticaPdf = ({
                 Diferencia Presupuesto/Egresos
               </Text>
             </View>
-            {ingresoMensualConPorcentaje.map((item) => {
-              if (item.tipo.startsWith("canjes")) {
-                // Si el tipo comienza con "canjes", no mostrar la fila
-                return null;
-              }
+            {ingresoMensualConPorcentaje
+              .filter((item) => !item.tipo.startsWith("canjes")) // Filtrar elementos que no comienzan con "canjes"
+              .sort((a, b) => b.total - a.total) // Ordenar los elementos por el total de forma descendente
+              .map((item) => {
+                // Buscar el objeto correspondiente en presupuestoMensualConPorcentaje y diferenciaPorTipo
+                const presupuestoItem = presupuestoMensualConPorcentaje.find(
+                  (presupuesto) => presupuesto.tipo === item.tipo
+                );
 
-              // Buscar el objeto correspondiente en presupuestoMensualConPorcentaje y diferenciaPorTipo
-              const presupuestoItem = presupuestoMensualConPorcentaje.find(
-                (presupuesto) => presupuesto.tipo === item.tipo
-              );
+                const diferenciaItem = diferenciaPorTipo.find(
+                  (diferencia) => diferencia.tipo === item.tipo
+                );
 
-              const diferenciaItem = diferenciaPorTipo.find(
-                (diferencia) => diferencia.tipo === item.tipo
-              );
-
-              return (
-                <View key={item.tipo} style={styles.tableRow}>
-                  <Text
-                    style={[
-                      styles.tableCell,
-                      {
-                        width: "15%",
-                        fontSize: "8px",
-                        fontFamily: "Montserrat",
-                        fontWeight: "bold",
-                        textTransform: "capitalize",
-                      },
-                    ]}
-                  >
-                    {item.tipo}
-                  </Text>
-                  <Text
-                    style={[
-                      styles.tableCell,
-                      {
-                        width: "20%",
-                        fontSize: "8px",
-                        fontFamily: "Montserrat",
-                        fontWeight: "bold",
-                      },
-                    ]}
-                  >
-                    {new Intl.NumberFormat("es-AR", {
-                      style: "currency",
-                      currency: "ARS",
-                    }).format(presupuestoItem?.total || 0)}
-                  </Text>
-                  <Text
-                    style={[
-                      styles.tableCell,
-                      {
-                        width: "15%",
-                        fontSize: "8px",
-                        fontFamily: "Montserrat",
-                        fontWeight: "bold",
-                      },
-                    ]}
-                  >
-                    {(presupuestoItem?.porcentajeUsado || 0).toFixed(2)}%
-                  </Text>
-                  <Text
-                    style={[
-                      styles.tableCell,
-                      {
-                        width: "20%",
-                        fontSize: "8px",
-                        fontFamily: "Montserrat",
-                        fontWeight: "bold",
-                      },
-                    ]}
-                  >
-                    {new Intl.NumberFormat("es-AR", {
-                      style: "currency",
-                      currency: "ARS",
-                    }).format(item.total)}
-                  </Text>
-                  <Text
-                    style={[
-                      styles.tableCell,
-                      {
-                        width: "15%",
-                        fontSize: "8px",
-                        fontFamily: "Montserrat",
-                        fontWeight: "bold",
-                      },
-                    ]}
-                  >{`${(item.porcentajeUsado || 0).toFixed(2)}%`}</Text>
-                  <Text
-                    style={[
-                      styles.tableCell,
-                      {
-                        width: "20%",
-                        fontSize: "8px",
-                        fontFamily: "Montserrat",
-                        fontWeight: "bold",
-                      },
-                      diferenciaItem?.diferencia < 0 ? { color: "red" } : {},
-                    ]}
-                  >
-                    {new Intl.NumberFormat("es-AR", {
-                      style: "currency",
-                      currency: "ARS",
-                    }).format(diferenciaItem?.diferencia || 0)}
-                  </Text>
-                </View>
-              );
-            })}
+                return (
+                  <View key={item.tipo} style={styles.tableRow}>
+                    <Text
+                      style={[
+                        styles.tableCell,
+                        {
+                          width: "15%",
+                          fontSize: "8px",
+                          fontFamily: "Montserrat",
+                          fontWeight: "bold",
+                          textTransform: "capitalize",
+                        },
+                      ]}
+                    >
+                      {item.tipo}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.tableCell,
+                        {
+                          width: "20%",
+                          fontSize: "8px",
+                          fontFamily: "Montserrat",
+                          fontWeight: "bold",
+                        },
+                      ]}
+                    >
+                      {new Intl.NumberFormat("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                      }).format(presupuestoItem?.total || 0)}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.tableCell,
+                        {
+                          width: "15%",
+                          fontSize: "8px",
+                          fontFamily: "Montserrat",
+                          fontWeight: "bold",
+                        },
+                      ]}
+                    >
+                      {(presupuestoItem?.porcentajeUsado || 0).toFixed(2)}%
+                    </Text>
+                    <Text
+                      style={[
+                        styles.tableCell,
+                        {
+                          width: "20%",
+                          fontSize: "8px",
+                          fontFamily: "Montserrat",
+                          fontWeight: "bold",
+                        },
+                      ]}
+                    >
+                      {new Intl.NumberFormat("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                      }).format(item.total)}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.tableCell,
+                        {
+                          width: "15%",
+                          fontSize: "8px",
+                          fontFamily: "Montserrat",
+                          fontWeight: "bold",
+                        },
+                      ]}
+                    >
+                      {`${(item.porcentajeUsado || 0).toFixed(2)}%`}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.tableCell,
+                        {
+                          width: "20%",
+                          fontSize: "8px",
+                          fontFamily: "Montserrat",
+                          fontWeight: "bold",
+                        },
+                        diferenciaItem?.diferencia < 0 ? { color: "red" } : {},
+                      ]}
+                    >
+                      {new Intl.NumberFormat("es-AR", {
+                        style: "currency",
+                        currency: "ARS",
+                      }).format(diferenciaItem?.diferencia || 0)}
+                    </Text>
+                  </View>
+                );
+              })}
           </View>
         </View>
 
@@ -557,3 +557,81 @@ export const ImprimirEstadisticaPdf = ({
     </Document>
   );
 };
+
+//
+import React from "react";
+import {
+  Document,
+  Page,
+  View,
+  Text,
+  StyleSheet,
+  Font,
+  Image,
+} from "@react-pdf/renderer";
+
+import logo from "../../../public/logo.png";
+import normal from "../../fonts/Montserrat-Light.ttf";
+import semibold from "../../fonts/Montserrat-SemiBold.ttf";
+import bold from "../../fonts/Montserrat-Bold.ttf";
+import { FixedSizeList } from "react-window";
+import { Bar } from "react-chartjs-2";
+
+Font.register({
+  family: "Montserrat",
+  fonts: [
+    {
+      src: normal,
+    },
+    {
+      src: semibold,
+      fontWeight: "semibold",
+    },
+    {
+      src: bold,
+      fontWeight: "bold",
+    },
+  ],
+});
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+  },
+  table: {
+    display: "table",
+    // width: "auto",
+  },
+  tableRow: { margin: "auto", flexDirection: "row" },
+  tableCell: {
+    margin: "auto",
+    marginTop: 5,
+    fontSize: 10,
+  },
+  tableHeader: {
+    margin: "auto",
+    marginTop: 5,
+  },
+  section: {
+    margin: 10,
+    padding: 10,
+    flexGrow: 1,
+  },
+  chartContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: "20px",
+    height: 200, // Reducimos la altura del contenedor del gráfico
+    marginTop: 20,
+  },
+  bar: {
+    width: 20, // Reducimos el ancho de las barras
+    backgroundColor: "#8884d8",
+    margin: 2,
+  },
+  label: {
+    fontSize: 8,
+    textAlign: "center",
+    marginTop: 2,
+  },
+});
